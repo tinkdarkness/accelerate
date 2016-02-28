f6ac421d510f00a32630980ade3bb824e3e4ee6c
tinkdarkness <tinkdarkness@gmail.com>
2016-02-27 21:06:37 -0500

    add custom post type

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -1,15 +1,12 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying case studies
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * 
  *
  * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @subpackage  Accelerate Theme
+ * @since Accelerate Theme 1.1
  */
 
 get_header(); ?>
@@ -17,7 +14,9 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
+			<article class="case-study">
 				<?php the_content(); ?>
+				</article>
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
