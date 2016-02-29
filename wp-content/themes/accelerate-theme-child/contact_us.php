4700172c6beac7eded7440bd9560acb9c100a4ca
tinkdarkness <tinkdarkness@gmail.com>
2016-02-29 09:43:50 -0500

    added Contact us page and ninja forms and customized.

---
+++ wp-content/themes/accelerate-theme-child/contact_us.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying Contact Us Page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Theme
+ * @since Accelerate Theme 1.1
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+		
+		<?php while ( have_posts() ) : the_post(); 
+			$name = get_field('name');
+			$email = get_field('email');
+			$your_message = get_field('your message');?>
+			
+		<?php while ( have_posts() ) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
